9f838c252840a6e8d1da31d2973a91b7a495e14d
gordonguthrie <gordonguthrie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-10 08:09:00 +0000

    Tagging Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/vixo-embeddable-tables-charts-and-spreadsheets/tags/1.0@636463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ vixo-shortcode.php
@@ -0,0 +1,58 @@
+<?php
+
+// define the plugin URL
+define('VIXO_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+// add the shortcode handler to available shortcodes
+add_shortcode('vixo', array('vixo_shortcode', 'shortcode'));
+
+// this is the callback class that will be used on rendering the page
+class vixo_shortcode {
+      function shortcode($atts, $content=null) {
+      	       // extract(shortcode_atts(array('url' => ''), $atts));
+      	       extract(shortcode_atts(array('url' => '', 'width' => '', 'height' => ''), $atts));
+	       if (empty($url)) return '<!--No Vixo Url -->';
+
+	       $h = "";
+	       $w = "";
+	       $resizeFlag = "";
+
+	       if (!empty($height)) {
+	         $h = "height:$height;";
+                 $resizeFlag = "hn_dont_resize";
+	       }
+
+	       if (!empty($width)) {
+	         $w = "width:$width;";
+		 $resizeFlag = "hn_dont_resize";
+	       }
+
+	       // concatenate the strings
+	       $style = "border:0;display:none;" . $h . $w;
+
+	       // add the custom javascript and css
+	       vixo_load_css_and_javascript();
+
+	       // now return the html
+	       $page = utf8_uri_encode(get_permalink());
+	       $name = uniqid();
+	       return "<iframe id='$name' class='hn_wordpress $resizeFlag' style='$style' src='$url#wordpress!$page!$name'></iframe>";
+	       }
+}
+
+// we need to load customer js and css - this function will be called to do it
+// when the shortcode is rendered
+function vixo_load_css_and_javascript() {
+
+ 	 # CSS first
+	 wp_register_style('vixo.wordpress.css', VIXO_PLUGIN_URL . 'vixo.wordpress.css');
+	 wp_enqueue_style('vixo.wordpress.css');
+
+	 # Now JS
+	 wp_register_script('vixo.wordpress.js', VIXO_PLUGIN_URL . 'vixo.wordpress.js', false, "", true);
+	 wp_enqueue_script('vixo.wordpress.js');
+	 wp_register_script('jquery.ba-postmessage.js', VIXO_PLUGIN_URL . 'jquery.ba-postmessage.js', false, "", true);
+	 wp_enqueue_script('jquery.ba-postmessage.js');
+}
+
+?>
